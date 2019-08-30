8c574efe996caf3f46e5d8bf66a2b9a897355f2b
fuping <962088984@qq.com>
2019-08-30 18:06:14 +0800

    18

--- coinbig/src/main/java/com/fp/util/propertyUtil.java
+++ coinbig/src/main/java/com/fp/util/propertyUtil.java
@@ -1,8 +1,25 @@
 package com.fp.util;
 
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
 /**
  * Created by Administrator on 2019/8/28 0028.
  */
 public class propertyUtil {
 
+    public static void loadProperties(){
+        ResourceBundle resource =  ResourceBundle.getBundle("config");
+        resource.getKeys();
+    }
+    public static void main(String[] args) {
+        ResourceBundle resource =  ResourceBundle.getBundle("config");
+        //String val = resource.getString("apiKey");
+        Enumeration enumeration = resource.getKeys();
+        if (enumeration.hasMoreElements()){
+            System.out.println("==="+enumeration.nextElement());
+        }
+
+       // System.out.println("---val:"+ val);
+    }
 }
